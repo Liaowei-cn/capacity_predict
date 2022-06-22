36c6afdfa343cf551098f902f2cf1a0a4d18af63
QuantaFu <1414729982@qq.com>
2022-06-22 15:10:34 +0800

    3.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "{'data':{['result':{[39, 34, 28, 33, 39, 36, 33, 31, 32, 33, 27, 27]}]}"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
